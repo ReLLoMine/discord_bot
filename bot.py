8970264e1992fa12914b355239d19805a5dacb9b
ReLLoMine <slaval.125.sl@gmail.com>
2020-10-07 21:05:37 +0300

    First version

---
+++ bot.py
@@ -0,0 +1,36 @@
+import discord
+import json
+
+
+class MyClient(discord.Client):
+    restricted_channel = 0
+    storage_file = None
+
+    def __init__(self):
+        super(MyClient, self).__init__()
+        self.storage_file = json.loads(open("storage.json").read())
+
+    async def on_ready(self):
+        print('Logged on as', self.user)
+        self.restricted_channel = open("")
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user and \
+                message.channel.id == 609054370415378473:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+
+def main():
+    token = 'NjA4NjY2MDIwNDg3MzY0NjEy.XlJgmw.cHtHW66U_zTP4jFp4rvOx_lvCsc'
+    client = MyClient()
+    # game = discord.Game("with the API")
+    # client.change_presence(status=discord.Status.idle, activity=game)
+    client.run(token)
+
+
+if __name__ == '__main__':
+    main()
